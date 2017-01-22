791d70a81546f0113f8cf59ad2454235213703e2
Dinarte Jesus <jdinartejesus@gmail.com>
2017-01-22 16:30:51 +0100

    Add BASE_URL const

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const { assert } = require('./utils')
+const BASE_URL = 'http://localhost:8080'
 
-http.get('/', (res) => {
+http.get(BASE_URL, (res) => {
   assert.equal(res.statusCode, 200, 'Status should be equal to 200')
 })
