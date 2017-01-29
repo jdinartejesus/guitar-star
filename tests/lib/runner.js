63b98dcaa93872dc9e5575be9a1d908713fafb77
Dinarte Jesus <jdinartejesus@gmail.com>
2017-01-29 23:38:58 +0100

    Refactor runner to test multiple tests

--- tests/lib/runner.js
+++ tests/lib/runner.js
@@ -1,9 +1,37 @@
 module.exports = {
-  test: function (message, fn) {
-    try {
-      fn()
-    } catch (error) {
-      throw new Error(error)
+  isSetup: false,
+  setup () {
+    this.isSetup = true
+    this.tests = []
+    this.errors = []
+    this.failures = 0
+
+    process.nextTick(() => {
+      this.run()
+      this.end()
+    })
+  },
+  test (message, fn) {
+    if (!this.isSetup) {
+      this.setup()
     }
+
+    this.tests.push(fn)
+  },
+  run () {
+    this.tests.forEach((test) => {
+      try {
+        test()
+      } catch (err) {
+        this.failures++
+        this.errors.push(err.message)
+      }
+    })
+  },
+  end () {
+    this.success = this.tests.length - this.failures
+
+    this.errors.forEach((error) => (process.stdout.write(`${error}\n`)))
+    process.stdout.write(`${this.failures} Failed ${this.success} Passed\n`)
   }
 }
