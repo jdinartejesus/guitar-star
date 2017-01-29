94ba28f17f9a3691f853288f3092ef2f2fe475e8
Dinarte Jesus <jdinartejesus@gmail.com>
2017-01-29 23:37:46 +0100

    Move assert to a separate method

--- tests/lib/assert.js
+++ tests/lib/assert.js
@@ -1,9 +1,11 @@
 module.exports = {
-  equal: function (value1, value2, message) {
-    message = message || "Error aren't equal"
-
-    if (value1 !== value2) {
-      throw new Error(message)
+  _assert (value, msg) {
+    if (!value) {
+      throw new Error(msg || `AssertionError: when evaluation ${value}`)
     }
+  },
+  equal (value1, value2, msg) {
+    msg = msg || `AssertionError: expected ${value1} to equal ${value2}`
+    this._assert(value1 === value2, msg)
   }
 }
