e3aa53e1085e0161e52c61c2821e76e021aada78
Dinarte Jesus <jdinartejesus@gmail.com>
2017-04-01 14:51:10 +0200

    Updated tests with new provoj version , closes #4

--- tests/test-api.js
+++ tests/test-api.js
@@ -6,12 +6,8 @@ const BASE_URL = 'http://localhost:8080'
 
 runner.test('/ok api should return 200 status code', (done) => {
   http.get(`${BASE_URL}/ok`, (res) => {
-    try {
-      assert.equal(res.statusCode, 200, 'Status should be equal to 200')
-      done()
-    } catch (err) {
-      done(err)
-    }
+    assert.equal(res.statusCode, 200, 'Status should be equal to 200', done)
+    done()
   }).on('error', (err) => {
     done(err)
   })
@@ -19,12 +15,8 @@ runner.test('/ok api should return 200 status code', (done) => {
 
 runner.test('/error api should return 500 status code', (done) => {
   http.get(`${BASE_URL}/error`, (res) => {
-    try {
-      assert.equal(res.statusCode, 500, 'Status should be equal to 500')
-      done()
-    } catch (err) {
-      done(err)
-    }
+    assert.equal(res.statusCode, 500, 'Status should be equal to 500', done)
+    done()
   }).on('error', (err) => {
     done(err)
   })
@@ -32,12 +24,8 @@ runner.test('/error api should return 500 status code', (done) => {
 
 runner.test('/foobar api should return 404 status code', (done) => {
   http.get(`${BASE_URL}/foobar`, (res) => {
-    try {
-      assert.equal(res.statusCode, 404, 'Status should be equal to 404')
-      done()
-    } catch (err) {
-      done(err)
-    }
+    assert.equal(res.statusCode, 404, 'Status should be equal to 404', done)
+    done()
   }).on('error', (err) => {
     done(err)
   })
