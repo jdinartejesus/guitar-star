bb7cc9e0e463da90a9b70e8af8a133bfc0362559
Dinarte Jesus <jdinartejesus@gmail.com>
2017-04-01 14:04:17 +0200

    Add new tests for the new endpoints

--- tests/test-api.js
+++ tests/test-api.js
@@ -4,10 +4,40 @@ let assert = Provoj.assert
 let runner = Provoj.runner
 const BASE_URL = 'http://localhost:8080'
 
-runner.test('base api should return 200 status code', (done) => {
-  http.get(BASE_URL, (res) => {
-    assert.equal(res.statusCode, 200, 'Status should be equal to 200')
-    done()
+runner.test('/ok api should return 200 status code', (done) => {
+  http.get(`${BASE_URL}/ok`, (res) => {
+    try {
+      assert.equal(res.statusCode, 200, 'Status should be equal to 200')
+      done()
+    } catch (err) {
+      done(err)
+    }
+  }).on('error', (err) => {
+    done(err)
+  })
+})
+
+runner.test('/error api should return 500 status code', (done) => {
+  http.get(`${BASE_URL}/error`, (res) => {
+    try {
+      assert.equal(res.statusCode, 500, 'Status should be equal to 500')
+      done()
+    } catch (err) {
+      done(err)
+    }
+  }).on('error', (err) => {
+    done(err)
+  })
+})
+
+runner.test('/foobar api should return 404 status code', (done) => {
+  http.get(`${BASE_URL}/foobar`, (res) => {
+    try {
+      assert.equal(res.statusCode, 404, 'Status should be equal to 404')
+      done()
+    } catch (err) {
+      done(err)
+    }
   }).on('error', (err) => {
     done(err)
   })
