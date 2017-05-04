d96a93efcd476dbe0b46a2a23f18c36950f6f831
Dinarte Jesus <jdinartejesus@gmail.com>
2017-05-04 09:21:45 +0200

    Implemented Endpoint for saving and fetching users, fix #5

--- src/server.js
+++ src/server.js
@@ -1,20 +1,47 @@
 const http = require('http')
+const fs = require('fs')
+var qs = require('querystring')
 
 const server = http.createServer((request, response) => {
-  console.log(request.url)
-
   switch (request.url) {
-    case '/ok':
-      response.writeHead(200, 'Success!')
-      break
-    case '/error':
-      response.writeHead(500, 'Ups!')
+    case '/users':
+      return getUsers((data) => {
+        response.writeHead(200, 'Success')
+        response.write(data)
+        response.end()
+      })
+    case '/user':
+      let body = []
+      request.on('data', (chunk) => (body.push(chunk)))
+      request.on('end', () => {
+        body = Buffer.concat(body).toString()
+        const res = qs.parse(body)
+        addUser(res, () => {
+          response.end()
+        })
+      })
       break
     default:
       response.writeHead(404, 'Not Found')
       break
   }
-  response.end()
 })
 
+function getUsers (cb) {
+  fs.readFile('src/db/users.txt', (err, data) => {
+    if (err) throw err
+
+    return cb(data)
+  })
+}
+
+function addUser (res, cb) {
+  fs.appendFile('src/db/users.txt', `${res.user}\n`, (err) => {
+    if (err) throw err
+
+    return cb()
+  })
+}
+
 server.listen(8080)
+
